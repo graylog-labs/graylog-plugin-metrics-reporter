b6f396f12ad72574c865216183a751042b132f8b
Alexander Campos <alexander.campos@mercadolibre.com>
2017-11-15 13:51:21 -0300

    Fix commentaries

--- metrics-reporter-cloudwatch/src/main/java/org/graylog/plugins/metrics/cloudwatch/MetricsCloudWatchReporterPlugin.java
+++ metrics-reporter-cloudwatch/src/main/java/org/graylog/plugins/metrics/cloudwatch/MetricsCloudWatchReporterPlugin.java
@@ -1,18 +1,18 @@
 /**
- * This file is part of Graylog Metrics Elasticsearch Reporter Plugin.
+ * This file is part of Graylog Metrics CloudWatch Reporter Plugin.
  *
- * Graylog Metrics Elasticsearch Reporter Plugin is free software: you can redistribute it and/or modify
+ * Graylog Metrics CloudWatch Reporter Plugin is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Graylog Metrics Elasticsearch Reporter Plugin is distributed in the hope that it will be useful,
+ * Graylog Metrics CloudWatch Reporter Plugin is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Graylog Metrics Elasticsearch Reporter Plugin.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Graylog Metrics CloudWatch Reporter Plugin.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.graylog.plugins.metrics.cloudwatch;
 
